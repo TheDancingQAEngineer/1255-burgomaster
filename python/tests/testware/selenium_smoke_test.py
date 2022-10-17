afa5c1ccec666f007c90bb7378293ad7743a1ff6
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-17 18:05:19 +0300

    Selenium Smoke test passing (squashed)
    commit 018f2190f499158b3032ac51e5634676eabc8fb4
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 17:58:49 2022 +0300
    
        Removed redundant steps from workflow
    
    commit 9b9ef0945575267791e7fd5e4c0904a838547b7c
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 17:48:37 2022 +0300
    
        checking selenium workflow for redundant steps
    
    commit ee32d8d90ee6a52fbc55788969c9da5e788c937b
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 17:45:45 2022 +0300
    
        Tests run in headless mode, checking setup
    
    commit 6cd11e11eea42f4ebbcd86b0dedbe2e78ba0a563
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 17:43:54 2022 +0300
    
        Still fixing
    
    commit 87f16304d420e56ed3c792eddb9767e7cebd94b3
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 17:35:33 2022 +0300
    
        still fixing selenium smoke test
    
    commit 9c63564280ea1d4bb7db4a868c3a5f61d6e86bf1
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 17:34:17 2022 +0300
    
        trying to fix geckodriver error
    
    commit 65db529a914d42281172c62b5c56ca36b53aeb6a
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 13:38:57 2022 +0300
    
        fixing selenium_smoke_test.py
    
    commit 2a8ed2584466a138a5ebaaa24b1d14e6a1fb0056
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 12:51:19 2022 +0300
    
        fixing selenium_smoke_test.py
    
    commit 49c846ab5619996923a537affed080c3f6a11a0b
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 12:45:34 2022 +0300
    
        fixing python-smoke-test.yml
    
    commit 1948d2a176a6b67df0c07b83ed6c7828310d7d40
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 12:44:50 2022 +0300
    
        fixing python-smoke-test.yml
    
    commit 99eed4c2528c4221824287e050e7f628e80e77d9
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 12:43:43 2022 +0300
    
        edited python-smoke-test.yml, now should install Firefox and geckodriver
    
    commit 8ebb2c9be90374f3a90cdd1679cadbdd34fcc231
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 12:39:18 2022 +0300
    
        fixing python-smoke-test.yml
    
    commit 65c6a06ace40619cb019c384c18583c38c7d8a41
    Author: Yuriy Avsharov <thedancingqaengineer@gmail.com>
    Date:   Mon Oct 17 12:38:22 2022 +0300
    
        updated python-smoke-test.yml, should install Selenium now

--- python/tests/testware/selenium_smoke_test.py
+++ python/tests/testware/selenium_smoke_test.py
@@ -1,11 +1,23 @@
 import unittest
 
+from os import environ
 from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class SeleniumSmokeTest(unittest.TestCase):
 
+    def setUp(self):
+        options: FirefoxOptions = FirefoxOptions()
+        if "GITHUB_ACTIONS" in environ:
+            options.headless = True
+        self.browser: WebDriver = Firefox(options=options)
+        self.addCleanup(self.browser.quit)
+
     def test_can_run_firefox_and_quit(self):
-        browser: WebDriver = Firefox()
-        browser.quit()
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
