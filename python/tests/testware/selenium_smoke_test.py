9c63564280ea1d4bb7db4a868c3a5f61d6e86bf1
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-17 17:34:17 +0300

    trying to fix geckodriver error

--- python/tests/testware/selenium_smoke_test.py
+++ python/tests/testware/selenium_smoke_test.py
@@ -1,14 +1,21 @@
 import unittest
 
 from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 class SeleniumSmokeTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser: WebDriver = Firefox()
+        options: FirefoxOptions = FirefoxOptions()
+        options.log.level = "trace"
+        self.browser: WebDriver = Firefox(options=options, executable_path="/snap/bin/geckodriver")
         self.addCleanup(self.browser.quit)
 
     def test_can_run_firefox_and_quit(self):
         pass
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
