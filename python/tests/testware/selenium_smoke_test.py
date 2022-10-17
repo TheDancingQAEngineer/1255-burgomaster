2a8ed2584466a138a5ebaaa24b1d14e6a1fb0056
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-17 12:51:19 +0300

    fixing selenium_smoke_test.py

--- python/tests/testware/selenium_smoke_test.py
+++ python/tests/testware/selenium_smoke_test.py
@@ -6,6 +6,11 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 class SeleniumSmokeTest(unittest.TestCase):
 
+    def setUp(self):
+        self.browser: WebDriver = Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
     def test_can_run_firefox_and_quit(self):
-        browser: WebDriver = Firefox()
-        browser.quit()
+        pass
