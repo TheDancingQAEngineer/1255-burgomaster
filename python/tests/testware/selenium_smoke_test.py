377744885e4a548937547b41eeb81e741bfc35b8
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-18 15:49:05 +0300

    minor refactoring and minimal game test

--- python/tests/testware/selenium_smoke_test.py
+++ python/tests/testware/selenium_smoke_test.py
@@ -1,22 +1,12 @@
 import unittest
 
-from os import environ
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.remote.webdriver import WebDriver
+from python.testware.seleniumwrapper import SeleniumBaseTest
 
 
-class SeleniumSmokeTest(unittest.TestCase):
-
-    def setUp(self):
-        options: FirefoxOptions = FirefoxOptions()
-        if "GITHUB_ACTIONS" in environ:
-            options.headless = True
-        self.browser: WebDriver = Firefox(options=options)
-        self.addCleanup(self.browser.quit)
+class SeleniumSmokeTest(SeleniumBaseTest):
 
     def test_can_run_firefox_and_quit(self):
-        pass
+        self.assertEqual("firefox", self.browser.name)
 
 
 if __name__ == '__main__':
