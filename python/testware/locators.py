770fdabb2f88f1db099ef41201610ce7366e7054
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-19 18:36:47 +0300

    refactoring to PageObject pattern

--- python/testware/locators.py
+++ python/testware/locators.py
@@ -2,4 +2,6 @@ from selenium.webdriver.common.by import By
 
 class StartPageLocators:
 
-    MENU_PANEL = (By.CLASS_NAME, ".menu-panel")
+    MENU_PANEL = (By.CLASS_NAME, "menu-panel")
+    RESOURCE_PANEL = (By.CLASS_NAME, "resource-panel")
+    CANVAS = (By.ID, "canvas")
