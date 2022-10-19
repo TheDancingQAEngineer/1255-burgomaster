d87c174376e9c815b2ba3a5bc6accf23fa372552
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-10-19 18:36:47 +0300

    refactoring to PageObject pattern

--- python/tests/func/starter_tests.py
+++ python/tests/func/starter_tests.py
@@ -1,45 +1,20 @@
-import unittest
 import warnings
 
 from python.testware.pageobjects import StartPageObject
 from python.testware.seleniumwrapper import SeleniumBaseTest
-from python.testware.timeouts import Timeouts
-
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 
 
 class StarterTests(SeleniumBaseTest):
 
-    def test_game_loads(self):
-        self.browser.get("http://localhost:6699")
-        
-        # Wait for menu panel to be visible
-        try:
-            WebDriverWait(self.browser, Timeouts.SHORT).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, "menu-panel")))
-        except TimeoutException as e:
-            raise AssertionError("Menu panel not visible after timeout.") from e
-        
-        # Wait for resource panel to be visible
-        try:
-            WebDriverWait(self.browser, Timeouts.SHORT).until(
-                EC.visibility_of_element_located((By.CLASS_NAME, "resource-panel")))
-        except TimeoutException as e:
-            raise AssertionError("Resource panel not visible after timeout.") from e
+    def setUp(self):
+        return super().setUp()
 
-        # Wait for canvas to be visible
-        try:
-            WebDriverWait(self.browser, Timeouts.SHORT).until(
-                EC.visibility_of_element_located((By.ID, "canvas")))
-        except TimeoutException as e:
-            raise AssertionError("Canvas not visible after timeout.") from e
+    def test_game_loads(self):
+        page = StartPageObject(self.browser)
+        self.browser.get("http://localhost:6699")
+
+        page.verify_menu_panel_is_visible()
+        page.verify_resource_panel_is_visible()
+        page.verify_canvas_is_visible()
 
         warnings.warn("This test needs refactoring!")
-        
-        
--- python/testware/pageobjects.py
+++ python/testware/pageobjects.py
@@ -1,42 +1,47 @@
-from typing import Tuple
+from python.testware.locators import StartPageLocators
+from python.testware.timeouts import Timeouts
+
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from python.testware.locators import StartPageLocators
-from python.testware.timeouts import Timeouts
+from typing import Tuple
 
 
 class BasePageObject:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, driver: WebDriver) -> None:
+        self._driver = driver
+
+    def verify_element_is_visible(self, locator: Tuple[By, str]):
+        try:
+            WebDriverWait(self._driver, Timeouts.SHORT).until(
+                EC.visibility_of_element_located(locator))
+        except TimeoutException as e:
+            raise AssertionError(f"Element \"{locator[1]}\" not visible after timeout.") from e
 
 
 class StartPageObject(BasePageObject):
     
     def __init__(self, driver: WebDriver) -> None:
-        super().__init__()
-        self._driver = driver
+        super().__init__(driver)
 
     def verify_menu_panel_is_visible(self):
         try:
-            self._wait_for_element_to_be_visible(StartPageLocators.MENU_PANEL)
+            self.verify_element_is_visible(StartPageLocators.MENU_PANEL)
         except AssertionError as e:
-            raise AssertionError("Menu panel not visible") from e
+            raise AssertionError("Menu panel not visible.") from e
 
-    def verify_resources_panel_is_visible(self):
-        raise NotImplementedError
+    def verify_resource_panel_is_visible(self):
+        try:
+            self.verify_element_is_visible(StartPageLocators.RESOURCE_PANEL)
+        except AssertionError as e:
+            raise AssertionError("Resource panel not visible.") from e
 
     def verify_canvas_is_visible(self):
-        raise NotImplementedError
-
-    def _wait_for_element_to_be_visible(self, locator: Tuple[By, str]):
         try:
-            element = WebDriverWait(self._driver, Timeouts.SHORT).until(
-                EC.visibility_of_element_located(locator)
-            )
-        finally:
-            raise AssertionError(f"Element not visible by locator: {locator[1]}.")
-
+            self.verify_element_is_visible(StartPageLocators.CANVAS)
+        except AssertionError as e:
+            raise AssertionError("Canvas not visible.") from e
