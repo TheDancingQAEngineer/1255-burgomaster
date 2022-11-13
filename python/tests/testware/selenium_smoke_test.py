9fcee6fea25f46fc4b098b9cfd7ff77c8d4904f9
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-11-13 20:32:06 +0300

    Added tests for chrome LocalStorage. Pass locally

--- python/tests/testware/selenium_smoke_test.py
+++ python/tests/testware/selenium_smoke_test.py
@@ -2,11 +2,36 @@ import unittest
 
 from python.testware.seleniumwrapper import SeleniumBaseTest
 
+from os import environ
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.command import Command
+
 
 class SeleniumSmokeTest(SeleniumBaseTest):
 
     def test_can_run_firefox_and_quit(self):
         self.assertEqual("firefox", self.browser.name)
+
+class SeleniumChromeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.options: ChromeOptions = ChromeOptions()
+        if "GITHUB_ACTIONS" in environ:
+            self.options.headless = True
+        self.options.add_experimental_option("w3c", False)
+        self.browser: WebDriver = Chrome(options=self.options)
+        self.addCleanup(self.browser.quit)
+
+    def test_can_run_chrome_and_quit(self):
+        self.assertEqual("chrome", self.browser.name)
+
+    def test_can_access_chrome_local_storage(self):
+        self.browser.get("http://localhost:6699")
+        storage_keys = self.browser.execute(Command.GET_LOCAL_STORAGE_KEYS)
+        self.assertIn("value", storage_keys.keys())
+        self.assertIn("areso-lang", storage_keys["value"])
 
 
 if __name__ == '__main__':
