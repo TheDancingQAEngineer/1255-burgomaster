a99968f56bbaf4ddd7b784d67accbd8517f619c6
Yuriy Avsharov <thedancingqaengineer@gmail.com>
2022-11-13 20:34:08 +0300

    WIP Local game load test - skeleton only

--- python/tests/func/load_save_tests.py
+++ python/tests/func/load_save_tests.py
@@ -4,13 +4,21 @@ from python.testware.seleniumwrapper import SeleniumBaseTest
 
 class LocalLoadSaveTest(SeleniumBaseTest):
     
-    def test_can_save_game(self):
+    def test_can_load_local_game(self):
+        # What is the right answer?
+        # How do I check if the answer is right?
+        
+        # GIVEN I have local save game
+        # WHEN I launch a game in browser
         page = StartPageObject(self.browser)
         self.browser.get("http://localhost:6699")
-        # Verify we're on game page
-        # Locate save game button and click
-        page.verify_save_button_is_present()
-        page.click_save_button()
-        # Should see log message "Save game successful"
+
+        # THEN I should see a Load Game button
+        # AND I should NOT see tutorial screen or other modal dialogs
+        
+        # WHEN I click Load Game button
+        
+        # THEN I should get a log message "Loaded Successfully"
+        # AND My stats should be restored from loaded game
         self.fail("Implement the test!")
     
